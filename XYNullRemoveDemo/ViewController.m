62d0e405cf25fc5ca3136fcefef055167a2f7fb0
xiaoyao <wokaoakkk@qq.com>
2016-05-20 09:21:13 +0800

    first commit

---
+++ XYNullRemoveDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XYNullRemoveDemo
+//
+//  Created by xiaoyao on 16/5/20.
+//  Copyright © 2016年 xiaoyao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ XYNullRemoveDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XYNullRemoveDemo
+//
+//  Created by xiaoyao on 16/5/20.
+//  Copyright © 2016年 xiaoyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
